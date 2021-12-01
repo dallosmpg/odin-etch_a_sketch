224395c1fc10c80d10dcd7dfb65950d4f2e627bf
TinyPin146 <tinypin14@gmail.com>
2021-12-01 23:22:17 +0100

    Added div-container DIV in. html. Created a for loop that creates 16x16 divs then appends them to container. Created 16x16 grid layout.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('It works')
+const divContainer = document.querySelector('.div-container')
+
+for (let i = 0; i < (16 * 16); i++) {
+    let newCell = document.createElement('div');
+    /* Naming the cell 1-256 */newCell.innerText = i + 1;
+    divContainer.appendChild(newCell).className = `grid-item grid-item-${i}`
+}
