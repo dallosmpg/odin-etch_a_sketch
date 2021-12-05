d203a8b7c023bb456b7a7910385ac4791b216025
TinyPin146 <tinypin14@gmail.com>
2021-12-05 22:39:55 +0100

    Added function coloring grid squares on hover. Added button and startd working on reset and resizing function onclick of said button.

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const divContainer = document.querySelector('.div-container')
 
-for (let i = 0; i < (16 * 16); i++) {
+const resetButton = document.querySelector('.reset');
+resetButton.addEventListener('click', function() {
+    createDivSquares(0);
+    let gridSquareNum = parseInt(prompt('Choose how many squares you want! (X*X)'));
+    divContainer.style.background='oldlace';
+    createDivSquares(gridSquareNum);
+})
+
+function createDivSquares(num = 16) {
+for (let i = 0; i < (num * num); i++) {
     let newCell = document.createElement('div');
     /* Naming the cell 1-256 */newCell.innerText = i + 1;
     divContainer.appendChild(newCell).className = `grid-item grid-item-${i}`
-}
+    }
+}   
+createDivSquares();
+const divElements = document.querySelectorAll('.grid-item');
+
+function colorOnHover(e) {
+   e.currentTarget.style.background='red';
+};
+
+divElements.forEach(element => element.addEventListener('mouseenter', colorOnHover))
+
