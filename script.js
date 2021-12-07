1c004bd86d7aec1a1f907618aa8c41561d0bb559
TinyPin146 <tinypin14@gmail.com>
2021-12-07 23:05:00 +0100

    Finished reset function with correct grid resizing. Modified coloring for random RGB value.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const root = document.documentElement;
 const divContainer = document.querySelector('.div-container')
 
 const resetButton = document.querySelector('.reset');
@@ -13,10 +14,12 @@ resetButton.addEventListener('click', function() {
 })
 
 function createDivSquares(num = 16) {
-for (let i = 0; i < (num * num); i++) {
-    let newCell = document.createElement('div');
-    /* Naming the cell 1-256 */newCell.innerText = i + 1;
-    divContainer.appendChild(newCell).className = `grid-item grid-item-${i}`
+    root.style.setProperty('--gtc', num);
+    root.style.setProperty('--gtr', num);
+    for (let i = 0; i < (num * num); i++) {
+        let newCell = document.createElement('div');
+        // /* Naming the cell 1-256 */newCell.innerText = i + 1;
+        divContainer.appendChild(newCell).className = `grid-item grid-item-${i}`
     }
 };
 createDivSquares();
@@ -26,7 +29,7 @@ return divElements;
 };
 
 function colorOnHover(e) {
-   e.currentTarget.style.background='red';
+   e.currentTarget.style.background=`rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
 };
 function red() {
 returnDivElements().forEach(element => element.addEventListener('mouseenter', colorOnHover))
