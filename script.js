1f8e44e0e4e8c149f8cdcea63af139f2ff11a11a
TinyPin146 <tinypin14@gmail.com>
2021-12-07 22:28:52 +0100

    Finished reset button functionality. Resizing works with overflow. Grid squares are reset and can be colored again. Prompt only accepts numbers above 1 else defaults to 16

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const divContainer = document.querySelector('.div-container')
 
 const resetButton = document.querySelector('.reset');
 resetButton.addEventListener('click', function() {
-    createDivSquares(0);
+    divContainer.innerHTML="";
     let gridSquareNum = parseInt(prompt('Choose how many squares you want! (X*X)'));
-    divContainer.style.background='oldlace';
-    createDivSquares(gridSquareNum);
+    createDivSquares(isNaN(gridSquareNum) ? 16
+    : gridSquareNum <= 1 ? 16
+    : gridSquareNum);
+    divContainer.insertAdjacentElement('afterbegin', resetButton);
+    returnDivElements()
+    red();
 })
 
 function createDivSquares(num = 16) {
@@ -14,13 +18,17 @@ for (let i = 0; i < (num * num); i++) {
     /* Naming the cell 1-256 */newCell.innerText = i + 1;
     divContainer.appendChild(newCell).className = `grid-item grid-item-${i}`
     }
-}   
+};
 createDivSquares();
+function returnDivElements() {
 const divElements = document.querySelectorAll('.grid-item');
+return divElements;
+};
 
 function colorOnHover(e) {
    e.currentTarget.style.background='red';
 };
-
-divElements.forEach(element => element.addEventListener('mouseenter', colorOnHover))
-
+function red() {
+returnDivElements().forEach(element => element.addEventListener('mouseenter', colorOnHover))
+};
+red();
