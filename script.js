c2116d6535006b9ee0a801e61ef837f16b0ff67d
TinyPin146 <tinypin14@gmail.com>
2021-12-08 22:10:12 +0100

    Added touchstart event listener. Touchscreens are also somewhat supported

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function colorOnHover(e) {
 
 function red() {
     returnDivElements().forEach(element => element.addEventListener('mouseenter', colorOnHover));
-    returnDivElements().forEach(element => element.addEventListener('touchenter', colorOnHover));
+    returnDivElements().forEach(element => element.addEventListener('touchstart', colorOnHover));
 };
 
 createDivSquares();
