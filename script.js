2ecc26daba35b04e730deb8b3c333f0deb3c37e0
Martin Dallos <tinypin14@gmail.com>
2023-01-27 17:58:42 +0100

    Site and function overhaul

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
 const root = document.documentElement;
 const divContainer = document.querySelector('.div-container')
-
 const resetButton = document.querySelector('.reset');
-resetButton.addEventListener('click', function() {
-    divContainer.innerHTML="";
-    let gridSquareNum = parseInt(prompt('Choose how many squares you would like to have! (X*X)'));
+const newSizeInput = document.querySelector('#newSize');
+const newSizeButton = document.querySelector('.reset-color-size');
+const newColorInput = document.querySelector('#newColor');
+const newColorButton = document.querySelector('.set-new-color');
+
+let currentColor = '#a51d2d';
+let isMouseDown = false;
+
+newSizeButton.addEventListener('click', resetAndResizeBoard)
+
+newColorButton.addEventListener('click', function() {
+    currentColor = newColorInput.value;
+    coloringEventListeners();
+})
+
+resetButton.addEventListener('click', resetBoard)
+
+function resetAndResizeBoard() {
+    divContainer.innerHTML = "";
+    const gridSquareNum = newSizeInput.value;
     createDivSquares(isNaN(gridSquareNum) ? 16
     : gridSquareNum <= 1 ? 16
     : gridSquareNum >= 100 ? 16
     : gridSquareNum);
-    divContainer.insertAdjacentElement('afterbegin', resetButton);
-    returnDivElements()
-    red();
-})
+    returnDivElements();
+    coloringEventListeners();
+}
+function resetBoard() {
+    document.querySelectorAll('.grid-item').forEach(item => {
+        item.style.background = 'revert';
+    })
+}
 
-function createDivSquares(num = 16) {
+function createDivSquares(num = newSizeInput.value) {
     root.style.setProperty('--gtc', num);
     root.style.setProperty('--gtr', num);
     for (let i = 0; i < (num * num); i++) {
         let newCell = document.createElement('div');
         // /* Naming the cell 1-256 */newCell.innerText = i + 1;
         divContainer.appendChild(newCell).className = `grid-item grid-item-${i}`
-        red();
+        coloringEventListeners();
     }
 };
 
@@ -31,16 +51,24 @@ function returnDivElements() {
 };
 
 function colorOnHover(e) {
-    let events;
-    events += 1;
-    // e.currentTarget.style.background=`hsl(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 100)}%, ${50}%)`;
-    e.currentTarget.style.background=`rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
-    console.log(e);
+    // rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})
+    if (!isMouseDown) return;
+    e.currentTarget.style.background=`${currentColor}`
 };
 
-function red() {
+function coloringEventListeners() {
     returnDivElements().forEach(element => element.addEventListener('mouseenter', colorOnHover));
-    returnDivElements().forEach(element => element.addEventListener('touchstart', colorOnHover));
 };
 
+function checkMouseClick(e) {
+    if (e.type === 'mousedown' && e.button === 0) {
+        isMouseDown = true;
+    }
+    else {
+        isMouseDown = false
+    }
+}
+
+divContainer.addEventListener('mousedown', checkMouseClick);
+divContainer.addEventListener('mouseup', checkMouseClick);
 createDivSquares();
